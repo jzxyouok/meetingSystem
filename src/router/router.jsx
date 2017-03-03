a2637b0b5885852b991274cfcf427d99ea2acc30
lznism <lzguo@whu.edu.cn>
2017-03-03 18:03:37 +0800

    20170303

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,12 +4,16 @@ import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 import NavBar from '../components/navBar/navBar';
 import AddNewActForm from '../components/addNewAct/addNewAct';
 import ActList from '../components/activityList/index';
+import BaseInfo from '../components/BaseInfo/BaseInfo';
+import AttendInfo from '../components/AttendInfo/AttendInfo';
 
 const routes = (
 	<Route path="/" component={NavBar}>
 		<IndexRoute component={ActList}></IndexRoute>
 		<Route path="addNew" component={AddNewActForm}></Route>
+		<Route path="baseInfo" component={BaseInfo}></Route>
 		<Route path="meetingManager" component={ActList}></Route>
+		<Route path="attendees" component={AttendInfo}></Route>
 	</Route>
 );
 
