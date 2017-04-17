93e7250236b5545e7a2ca5d2c74438dafdbd0114
lznism <lzguo@whu.edu.cn>
2017-04-17 11:35:16 +0800

    删除表单的常用项

--- src/Component/register-form/Index.jsx
+++ src/Component/register-form/Index.jsx
@@ -5,7 +5,6 @@ import React, { Component } 		from 'react';
 import { Row, Col, Button, Icon, Tag, Tabs } 	
 									from 'antd';
 
-import CustomOptions 				from './custom-options';
 import AddCustomize 				from './add-item-operator';
 import CustomizeOptions 			from './customize-options';
 import FormHandler 					from './form-handler';
@@ -34,10 +33,6 @@ export default class RegisterForm extends Component {
 					<TabPane key="1" tab="报名表单">
 						<Row>
 							<FormState />
-							<Col {...row_item_layout} className="options" offset={4}>添加表单固有项</Col>
-						</Row>
-						<CustomOptions />
-						<Row>
 							<Col {...row_item_layout} className="options" offset={4}>添加表单自定义项</Col>
 						</Row>
 						<AddCustomize />
